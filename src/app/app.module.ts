76a36d70aa0660831e2448374fda74e3dbb06159
Ana Menezes <anavick.sk@gmail.com>
2024-03-10 22:20:23 -0300

    Create shopping list and recipes components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
-import { ShoppintEditComponent } from './shopping-list/shoppint-edit/shoppint-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipeBookComponent,
     ShoppingListComponent,
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
-    ShoppintEditComponent
+    RecipesComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
